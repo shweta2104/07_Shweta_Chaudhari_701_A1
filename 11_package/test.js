7e62ee1288bd157cc030ca25a2e109985ef62ea3
shweta2104 <sweetychaudhari1920@gmail.com>
2025-07-20 11:57:39 +0530

    Add files via upload

---
+++ 11_package/test.js
@@ -0,0 +1,5 @@
+const utils = require('shweta-string-utils');
+
+console.log(utils.capitalize("hello"));   // Hello
+console.log(utils.reverse("world"));      // dlrow
+console.log(utils.isPalindrome("madam")); // true
